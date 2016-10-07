1614c8b127e7b7ccb413779887b7b4be0bbb2ac9
cz-it <cz.theng@gmail.com>
2016-10-07 21:43:08 +0800

    main

---
+++ main.go
@@ -0,0 +1,18 @@
+/**
+* Author: CZ cz.devnet@gmail.com
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	fmt.Println("igen- Impress.js project generator")
+}
